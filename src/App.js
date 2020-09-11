f1528c2af06f453d8c07935ec424af8c53ca36c5
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-11 17:06:44 +0530

    Sider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Layout, Menu } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       isCollapsed: false,
+    }
+  }
+
+  onCollapse = isCollapsed => {
+    this.setState({ isCollapsed });
+  };
+  
+  render() {
+    return (
+      <div>
+        <Layout  style={{ minHeight: '100vh' }}>
+          <Sider width={300} collapsible collapsed={this.state.isCollapsed} onCollapse={this.onCollapse}>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />} />
+          </Menu>
+          </Sider>
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
