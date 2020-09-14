572678d8498809283b291c0e16db2f2150e1493b
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-14 20:21:17 +0530

    project tabpane

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import PageHeader from '../../Utils/PageHeader'
+import { Tabs } from 'antd';
+
+import './Projects.css'
+
+const { TabPane } = Tabs;
 
 export class Projects extends Component {
   render() {
@@ -8,6 +13,20 @@ export class Projects extends Component {
         <PageHeader 
           Title={<span>Projects</span>}
         />
+        <Tabs defaultActiveKey="1" centered>
+        <TabPane tab="All" key="1">
+            Content of Tab Pane 3
+          </TabPane>
+          <TabPane tab="Golang" key="2">
+            Content of Tab Pane 1
+          </TabPane>
+          <TabPane tab="React-Js" key="3">
+            Content of Tab Pane 2
+          </TabPane>
+          <TabPane tab="Python" key="4">
+            Content of Tab Pane 3
+          </TabPane>
+        </Tabs>
       </div>
     )
   }
