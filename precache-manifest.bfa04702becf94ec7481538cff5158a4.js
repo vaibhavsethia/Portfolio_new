9e6a0f4fe117f507f9da5ed20ce729e2c5c1aaa7
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-18 21:05:30 +0530

    Updates

---
+++ precache-manifest.bfa04702becf94ec7481538cff5158a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98f7d49d394eb2fd5d54180ad5c3aa0",
+    "url": "/Portfolio_new/index.html"
+  },
+  {
+    "revision": "149fba02a6a48c96ed59",
+    "url": "/Portfolio_new/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "a6ec7b3d6cdd69587450",
+    "url": "/Portfolio_new/static/css/main.f3223650.chunk.css"
+  },
+  {
+    "revision": "149fba02a6a48c96ed59",
+    "url": "/Portfolio_new/static/js/2.3cca659f.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Portfolio_new/static/js/2.3cca659f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ec7b3d6cdd69587450",
+    "url": "/Portfolio_new/static/js/main.8bac51c6.chunk.js"
+  },
+  {
+    "revision": "56984b809d6e48846ce4",
+    "url": "/Portfolio_new/static/js/runtime-main.e7582a21.js"
+  },
+  {
+    "revision": "2a4c098a6a3d24604f2ee2d110255f13",
+    "url": "/Portfolio_new/static/media/Avatar.2a4c098a.png"
+  },
+  {
+    "revision": "4d11c40b119d895bd3e18ae4e313ff07",
+    "url": "/Portfolio_new/static/media/Image_1.4d11c40b.jpeg"
+  }
+]);
