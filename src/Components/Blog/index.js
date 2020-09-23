267c5310867bac60887dd00adcc7caeb4db7954e
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-23 23:34:20 +0530

    blog player

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react'
-import PageHeader from '../../Utils/PageHeader'
-import ReactPlayer from 'react-player'
+import { Row } from "antd";
+import React, { Component } from "react";
+import PageHeader from "../../Utils/PageHeader";
+import SongCard from "../../Utils/SongCardd";
+import SongOfTheMonth from "../../Songs/SongOfTheMonth.mp3";
 
 export class Blog extends Component {
   render() {
     return (
       <div>
         <PageHeader Title={<span>Blog</span>} />
-        <ReactPlayer url="https://www.youtube.com/watch?v=olGSAVOkkTI" />
+        <PageHeader Title={<span>Song of the Month</span>} />
+        <Row justify="center">
+          <SongCard 
+          Album="Fine Line"
+          Song="Falling"
+          Image={SongOfTheMonth}
+          YT="https://www.youtube.com/watch?v=olGSAVOkkTI"
+          Singer="Harry Styles"
+          />
+        </Row>
       </div>
-    )
+    );
   }
 }
 
-export default Blog
+export default Blog;
---
+++ src/Utils/SongCard.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Carousel, Tooltip } from "antd";
+import ReactAudioPlayer from "react-audio-player";
+
+import "./Blog.css";
+import { YoutubeOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
+
+const contentStyle = {
+  backgroundImage: "./September.jpg",
+};
+
+export class SongCard extends Component {
+  state = {
+    dotPosition: "top",
+  };
+  render() {
+    return (
+      <div className="w5">
+        <Carousel className="shadow-2" effect="fade" dotPosition="top">
+          <div>
+            <div className="song-card pr2">
+              <Tooltip className="info-icon" title="Player not working ?? Try reloading the page">
+                <ExclamationCircleOutlined />
+              </Tooltip>
+              <span className="small-text-card">Album :</span>
+              <span className="big-text-card">{this.props.Album}</span>
+              <br />
+              <span className="small-text-card">Song :</span>
+              <span className="big-text-card">{this.props.Song}</span>
+              <br />
+              <span className="small-text-card">Singer :</span>
+              <span className="big-text-card">{this.props.Singer}</span>
+              <br />
+              <a href={this.props.YT}>
+                <YoutubeOutlined className="yt-icon" />
+              </a>
+            </div>
+            <ReactAudioPlayer src={September} autoPlay={true} controls />
+          </div>
+        </Carousel>
+      </div>
+    );
+  }
+}
+
+export default SongCard;
