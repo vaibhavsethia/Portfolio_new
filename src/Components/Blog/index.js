f7fcaf87466bc25b913bdec7087e4a4d3eca3d6a
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-23 20:04:30 +0530

    player

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import PageHeader from '../../Utils/PageHeader'
+import ReactPlayer from 'react-player'
 
 export class Blog extends Component {
   render() {
     return (
       <div>
         <PageHeader Title={<span>Blog</span>} />
+        <ReactPlayer url="https://www.youtube.com/watch?v=olGSAVOkkTI" />
       </div>
     )
   }
