31e043c4876163b812643878e2cea4374617472c
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-26 19:21:15 +0530

    timeline

--- src/Components/Timeline/index.js
+++ src/Components/Timeline/index.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Alert } from "antd";
 
 export class Timeline extends Component {
   render() {
     return (
       <div>
         Timeline
+        <Alert
+          classname="pa4"
+          message="Section still in Building phase"
+          type="warning"
+        />
       </div>
-    )
+    );
   }
 }
 
-export default Timeline
+export default Timeline;
